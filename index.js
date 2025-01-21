e181c5c10a2c6d5ac9c37fa4a98e889b5ebc2247
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-01-21 05:14:39 -0500

    added greet function

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Your code here
 
 function greet() {
+    let name = window.prompt("What is your name?");
+    window.alert("Hello, " + name);
 
-    
+    let age = window.prompt("How old are you?");
+    age = parseInt(age);
+
+    let year = new Date().getFullYear();
+
+    if (window.confirm("Have you had a birthday yet this year?")) {
+        let birthYear = year - age;
+        window.alert("You were born in " + birthYear);
+    } else {
+        let birthYear = year - (age + 1);
+        window.alert("You were born in " + birthYear);
+    }
 }
